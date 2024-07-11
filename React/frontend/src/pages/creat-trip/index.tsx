395ca15f28bafecd39fbcdb145a852a192553361
Gabrielygor <gabrielcanuto042@gmail.com>
2024-07-11 17:04:06 -0300

    Modularização do creatTrip

--- React/frontend/src/pages/creat-trip/index.tsx
+++ React/frontend/src/pages/creat-trip/index.tsx
@@ -1,8 +1,9 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react'
 import { FormEvent, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { InviteGuestModal } from './invite-guests-modal'
 import { ComfirmTripModal } from './confirm-trip-modal'
+import { DestinationAndDateStep } from './steps/destination-and-date-step'
+import { InviteGuestStep } from './steps/invite-guets-step'
 
 
 export function CreateTripPage() {
@@ -57,7 +58,6 @@ export function CreateTripPage() {
 
         setEmailsToInvite([
             ...emailsToInvite,
-
             email
         ])
 
@@ -88,53 +88,18 @@ export function CreateTripPage() {
                 </div>
 
                 <div className='space-y-4'>
-                    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-5">
-                        <div className='flex items-center gap-2 flex-1'>
-                            <MapPin className='size-5 text-zinc-400' />
-                            <input disabled={isGuestInputOpen} type="text" placeholder="Para onde você vai?" className=" bg-transparent text-lg placeholder-zinc-400 outline-none" />
-                        </div>
-
-                        <div className='flex items-center gap-2 flex-1'>
-                            <Calendar className='size-5 text-zinc-400' />
-                            <input disabled={isGuestInputOpen} type="text" placeholder="Quando?" className=" bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-                        </div>
-
-                        <div className='w-px h-6 bg-zinc-800'></div>
-
-                        {isGuestInputOpen ? (
-                            <button onClick={closeGuestsInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 flex items-center gap-2 hover:bg-zinc-700'>Alterar local/data
-                                <Settings2 className='size-5' />
-                            </button>
-                        ) : (
-                            <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 flex items-center gap-2 hover:bg-lime-400'>
-                                Continuar
-                                <ArrowRight className='size-5' />
-                            </button>
-                        )
-                        }
-
-                    </div>
+                    <DestinationAndDateStep
+                        closeGuestsInput={closeGuestsInput}
+                        openGuestsInput={openGuestsInput}
+                        isGuestInputOpen={isGuestInputOpen}
+                    />
 
                     {isGuestInputOpen && (
-                        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-5">
-                            <button type='button' onClick={openGuestsModal} className='flex items-center gap-2 flex-1 text-left'>
-                                <UserRoundPlus className='size-5 text-zinc-400' />
-                                {emailsToInvite.length > 0 ? (
-                                    <span className='text-zinc-100 text-lg flex-1'>{emailsToInvite.length} pessoa(s) convidada(s)</span>
-
-                                ) : (
-                                    <span className='text-zinc-400 text-lg flex-1'>Quem estará na viagem?</span>
-                                )}
-
-                            </button>
-
-                            <div className='w-px h-6 bg-zinc-800'></div>
-
-                            <button onClick={openConfirmTripModal} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 flex items-center gap-2 hover:bg-lime-400'>
-                                Comfirmar viagem
-                                <ArrowRight className='size-5' />
-                            </button>
-                        </div>
+                        <InviteGuestStep
+                            emailsToInvite={emailsToInvite}
+                            openConfirmTripModal={openConfirmTripModal}
+                            openGuestsModal={openGuestsModal}
+                        />
 
                     )}
                 </div>
